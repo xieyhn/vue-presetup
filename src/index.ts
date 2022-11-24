406293bfc89e13b3b00686016716ad327dc578ee
xieyuhang <13451213869@163.com>
2022-11-24 18:50:15 +0800

    feat: api removeComponent

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,7 @@ import type { VNode, PropType, Component } from 'vue'
 type Props = Record<string, any>
 type Callback = (...args: any[]) => void
 
-interface Context {
-  vnode: VNode
+interface PresetupContext {
   props: Props
   resolve: Callback
   reject: Callback
@@ -22,7 +21,10 @@ const COMPONENT_KEPT_ALIVE = 1 << 9
 const CONTEXT_KEY = Symbol('VUE_PRESETUP_CONTEXT_KEY')
 const NOOP = () => {}
 
-const cache = new Map<string, VNode>()
+const cache = new Map<string, {
+  vnode: VNode,
+  container: HTMLElement
+}>()
 
 export const PresetupView = defineComponent({
   props: {
@@ -43,9 +45,9 @@ export const PresetupView = defineComponent({
       const name = typeof component.type === 'object' ? (component.type as Component).name : ''
       if (!name || !cache.has(name))
         return component
-      const vnode = cache.get(name)!
+      const data = cache.get(name)!
       cache.delete(name)
-      return vnode
+      return data.vnode
     }
   }
 })
@@ -54,7 +56,7 @@ export function useContext(): UseContextResult {
   const instance = getCurrentInstance()
   if (!instance) 
     throw new Error(`[vue-presetup] Not found \`currentInstance\``)
-  const context = (Reflect.get(instance.vnode, CONTEXT_KEY) ?? {}) as Partial<Context>
+  const context = (Reflect.get(instance.vnode, CONTEXT_KEY) ?? {}) as Partial<PresetupContext>
 
   const get: UseContextResult['get'] = (key, defaultValue?) => {
     const props = context?.props ?? {}
@@ -77,14 +79,34 @@ export function setupComponent(component: Component, props: Record<string, any> 
       reject(new Error(`[vue-presetup] Missing Component \`name\` attribute.`))
       return
     }
+    const container = window.document.createElement('div')
     const vnode = h(component, props)
-    cache.set(component.name, vnode)
+    const context: PresetupContext = { props, resolve, reject }
+
+    if (cache.has(component.name)) removeComponent(component.name)
+    cache.set(component.name, { vnode, container })
+
     Reflect.defineProperty(vnode, CONTEXT_KEY, {
-      value: { vnode, props, resolve, reject } as Context,
+      value: context,
       configurable: true,
       writable: true
     })
-    render(vnode, window.document.createElement('div'))
+
+    render(vnode, container)
     vnode.shapeFlag |= COMPONENT_KEPT_ALIVE
   })
+}
+
+export function removeComponent(name: string) {
+  const data = cache.get(name)
+  if (!data) {
+    if (process.env.NODE_ENV === 'development') {
+      console.warn(
+        `[vue-presetup] No component with name \`${name}\` found.`
+      )
+    }
+    return
+  }
+  render(null, data.container)
+  cache.delete(name)
 }
